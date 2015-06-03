08d7166d51ebb6dcef578176213b53ed5cd18d52
Egor Pushkin <egor@glympse.com>
2015-06-02 20:51:50 -0700

    And build script.

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# App name/home registry
+REGISTRY_URL=registry.enterprise.glympse.com
+IMAGE_NAME=tools/docker-cluster
+
+# Helpers
+IMAGE_TAG=$(git rev-parse --short HEAD)
+TAGGED_IMAGE=$REGISTRY_URL/$IMAGE_NAME:$IMAGE_TAG
+
+# Build the image
+docker build -t $IMAGE_NAME .
+
+# Mark image with registry URL and tag
+docker tag -f $IMAGE_NAME $TAGGED_IMAGE
+
+# Push image to repote registry
+docker push $TAGGED_IMAGE
