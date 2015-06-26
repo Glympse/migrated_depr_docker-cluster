91fc0e677167a2f619eee87de0cff5b8045dff76
Egor Pushkin <egor@glympse.com>
2015-06-25 22:03:09 -0700

    Horizontal layout is supported.

--- client/src/app/hosts/hosts.controller.js
+++ client/src/app/hosts/hosts.controller.js
@@ -6,6 +6,8 @@
         .controller('HostsController', HostsController);
 
     function HostsController($scope, hostsService) {
+        $scope.layout_horizontal = true;
+
         hostsService.getList().success(function(data) {
             $scope.hosts = data.body;
         });
