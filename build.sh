25c5d588929b69c25a9effad998a80cbe26e5d56
Egor Pushkin <egor@glympse.com>
2015-10-10 17:00:07 -0700

    Build file points to Docker Hub.

--- build.sh
+++ build.sh
@@ -1,21 +1,14 @@
 #!/bin/sh
 
-# App name/home registry
-REGISTRY_URL=registry.sandbox.glympse.com
-IMAGE_NAME=tools/docker-cluster
-
-# Helpers
-IMAGE_TAG=$(git rev-parse --short HEAD)
-TAGGED_IMAGE=$REGISTRY_URL/$IMAGE_NAME:$IMAGE_TAG
-LATEST_IMAGE=$REGISTRY_URL/$IMAGE_NAME:latest
+# App name
+IMAGE_NAME=glympse/docker-cluster
+LATEST_IMAGE=$IMAGE_NAME:latest
 
 # Build the image
 docker build -t $IMAGE_NAME .
 
 # Mark image with registry URL and tag
-docker tag -f $IMAGE_NAME $TAGGED_IMAGE
 docker tag -f $IMAGE_NAME $LATEST_IMAGE
 
 # Push image to repote registry
-docker push $TAGGED_IMAGE
 docker push $LATEST_IMAGE
